47dd88eb06d556aa94fce663022bb9a4c9b96f2f
陈玲钰 <chenlingyu@innotechx.com>
2021-03-02 15:51:34 +0800

    feat: header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,22 @@ import styles from './styles.module.less';
 
 export const Header = () => {
   return (
-    <Row className={styles.contain}>
-      <Col span="20"></Col>
-      <Col span="4" className={styles.user}>
-        <span className={styles.userName}>欢迎，Joan</span>
-        <Button>退出</Button>
-      </Col>
-    </Row>
+    <div>
+      <Row className={styles.userWrap}>
+        <Col span="20"></Col>
+        <Col span="4" className={styles.user}>
+          <span className={styles.userName}>欢迎，Joan</span>
+          <Button>退出</Button>
+        </Col>
+      </Row>
+      <Row className={styles.header}>
+        <Col span="4" className={styles.breadcrumb}>
+          首页
+        </Col>
+        <Col span="20" className={styles.weather}>
+          天气
+        </Col>
+      </Row>
+    </div>
   );
 };
